50403ac5e91271dcdb1a11aa1af475a231489d75
ta4ilka69 <artem_balin_2004@mail.ru>
2024-10-23 20:52:08 +0300

    Repositories fixed)

--- back/src/main/java/itmo/labs/Repository/CoordinatesRepository.java
+++ back/src/main/java/itmo/labs/Repository/CoordinatesRepository.java
@@ -1,9 +1,10 @@
 package itmo.labs.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import itmo.labs.Model.Coordinates;
-
+@Repository
 public interface CoordinatesRepository extends JpaRepository<Coordinates,Integer>{
 
 }
--- back/src/main/java/itmo/labs/Repository/LocationRepository.java
+++ back/src/main/java/itmo/labs/Repository/LocationRepository.java
@@ -1,9 +1,10 @@
 package itmo.labs.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import itmo.labs.Model.Location;
-
+@Repository
 public interface LocationRepository extends JpaRepository<Location,Integer>{
 
 }
