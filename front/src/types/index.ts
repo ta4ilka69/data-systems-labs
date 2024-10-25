eb77d9d72ffffcba55bb25d6d430a6b02775acb6
ta4ilka69 <artem_balin_2004@mail.ru>
2024-10-25 16:05:24 +0300

    starting front

---
+++ front/src/types/index.ts
@@ -0,0 +1,44 @@
+export interface RouteDTO {
+  id?: number;
+  name: string;
+  coordinates: CoordinatesDTO;
+  creationDate?: string;
+  from: LocationDTO;
+  to?: LocationDTO;
+  distance?: number;
+  rating: number;
+  createdById?: number;
+  createdByUsername?: string;
+  allowAdminEditing: boolean;
+}
+
+export interface CoordinatesDTO {
+  x: number;
+  y: number;
+}
+
+export interface LocationDTO {
+  x: number;
+  y: number;
+  name: string;
+}
+
+export interface AuthRequestDTO {
+  username: string;
+  password: string;
+}
+
+export interface AuthResponseDTO {
+  token: string;
+}
+
+export interface RegisterRequestDTO {
+  username: string;
+  password: string;
+}
+
+export interface RegisterResponseDTO {
+  id: number;
+  username: string;
+  roles: string[];
+}
